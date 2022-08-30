dc8c5d8440ea2e3362ca520bdd5fa17fd042d0a8
광주2반_용승민 <yongsm295@gmail.com>
2022-08-30 17:59:37 +0900

    0830 수업끗

--- 01 python/test/test.py
+++ 01 python/test/test.py
@@ -1,6 +1,20 @@
-year = int(input())
+p = 'is'
+t = 'this is a book~!'
+M = len(p)
+N = len(t)
 
-if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    print(1)
-else:
-    print(0)
+
+def BruteForce(p, t):
+    i = 0 #t의 인덱스
+    j = 0 #p의 인덱스
+    while j < M and i < N :
+        if t[i] != p[j]:
+            i = i = j
+            j = -1
+        i = i + 1
+        j = j + 1
+    if j == M : return i - M #검색 성공!
+    else : return -1 #검색 실패
+
+
+print(BruteForce(p, t))
